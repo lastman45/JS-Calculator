c097781ed34d4c0ea5a156428321cb637d3644dc
okey8liver <167875491+lastman45@users.noreply.github.com>
2025-09-23 10:16:42 +0300

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,24 @@
+
+
+/*const display = document.getElementById("display");
+
+function appendToDisplay(input){
+   display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.vlaue = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error!!";
+    }
+}*/
+
+
+
+
